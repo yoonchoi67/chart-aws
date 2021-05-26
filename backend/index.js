435479f21958b78fcf690d1bda7452a97c52aa87
Yoon Choi <choijong@grinnell.edu>
2021-05-26 17:43:53 +0900

    server fix

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,13 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/routes.js';
+ 
 
-
+if(process.env.NODE_ENV === 'production') {
+    console.log("prod")
+} else {
+    console.log('dev')
+}
 
 const app = express();
 const port = process.env.PORT;
