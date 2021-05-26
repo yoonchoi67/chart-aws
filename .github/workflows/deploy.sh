c37c4c6fb658570ffce3354988f3b70e82ebe676
Yoon Choi <choijong@grinnell.edu>
2021-05-26 15:49:53 +0900

    first commit cicd

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+user="ubuntu"
+
+rm -rf .git
+rm -rf .gitignore
+git config --global user.email "choijong@grinnell.edu"
+git config --global user.name "Yoon Choi"
+mv .gitignore_cicd .gitignore
+git init .
+git add .
+git commit -m "Deploying"
+git remote add production ssh://$user@$AWS_HOST/~/webapp
+git push --force production master
+
+ssh $user@$AWS_HOST "cd ~/webapp && \
+pm2 kill
+NODE_ENV=production pm2 start /home/ubuntu/webapp/server/build/server.js
+exit"
