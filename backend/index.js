f6757fd08c5049eb34829cc383aa4f296d9e7bef
Yoon Choi <choijong@grinnell.edu>
2021-05-27 14:10:32 +0900

    no important

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,25 @@ import cors from 'cors';
 import routes from './routes/routes.js';
  
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 //take a look again
 const PRODUCTION = process.env.NODE_ENV === 'production'
+console.log(PRODUCTION)
 
 if( PRODUCTION ) {
-    app.use('/', express.static('/home/ubuntu/webapp/client/build'))
-} else {
-    app.use("/", routes);
+    app.use('/', express.static('/home/ubuntu/webapp/frontend/build'))
+} else {    
+    // app.use('/', express.static('./../frontend/src'))
+    app.use('/', express.static('./../frontend/build'))
+    // app.use("/", routes);
 }
 
 
 
 //take a look again
 const port = process.env.PORT;
-app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server Running at ${port}`)
