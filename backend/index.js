f336d08ff5ca058ef8c6b18f9f15d8a711d2134a
Yoon Choi <choijong@grinnell.edu>
2021-06-20 12:47:15 +0900

    url fix

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 // app.use(cors());
 app.use(express.json());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://http://ec2-54-180-26-58.ap-northeast-2.compute.amazonaws.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://ec2-54-180-26-58.ap-northeast-2.compute.amazonaws.com"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
