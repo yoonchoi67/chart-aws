834be3d832f87c3d6cf81b53d028f5a013d46847
Yoon Choi <choijong@grinnell.edu>
2021-06-20 16:35:06 +0900

    change prod to use correct path

--- backend/index.js
+++ backend/index.js
@@ -5,18 +5,13 @@ import routes from './routes/routes.js';
 // import helmet from 'helmet';
  
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://ec2-54-180-26-58.ap-northeast-2.compute.amazonaws.com"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  // app.use(helmet());
 
 //process.env.NODE_ENV is set in package.json
 if( process.env.NODE_ENV === 'production' ) {
-    app.use('/', express.static('/home/ubuntu/webapp/frontend/build'))
+    app.use('/', express.static('/home/ubuntu/webapp/frontend/build'));
+    app.use("/", routes);
 } else {    
     app.use("/", routes);
 }
